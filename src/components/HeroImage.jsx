c13f16f90702ddee4629c7818d8c0445b861fb5b
Ian Izaguirre <ian@izaguir.re>
2018-08-07 17:55:03 -0400

    Refactor: Added Hero image through CSS

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -21,20 +21,38 @@ const ImageContainer = styled.div`
   // // filter: blur(5px);
 `;
 
-const Image = styled.img`
-  display: block;
-  margin: 0 auto;
+// const Image = styled.img`
+//   display: block;
+//   margin: 0 auto;
 
-  max-width: 100%;
+//   max-width: 100%;
+//   min-height: 35em;
+//   height: auto;
+// `;
+
+// <Image alt="logo" src={`${APIConnect.imageUrl}original${this.props.heroImage}`} />;
+
+const Image = styled.div`
+  // display: block;
+  // margin: 0 auto;
+
+  // max-width: 100%;
   min-height: 35em;
-  height: auto;
+  // height: auto;
+
+  background-size: cover;
+  background-repeat: no-repeat;
 `;
+
+// background-image: url(https://image.tmdb.org/t/p/original/cfbjFQ14hSTgXChBEvaEjFiUaKb.jpg);
+
+// background-image: ${props => `url(${APIConnect.imageUrl}original${props.heroImage})`};
 
 class HeroImage extends Component {
   render() {
     return (
       <ImageContainer>
-        <Image alt="logo" src={`${APIConnect.imageUrl}original${this.props.heroImage}`} />
+        <Image style={{ backgroundImage: `url(${APIConnect.imageUrl}original${this.props.heroImage})` }} />
       </ImageContainer>
     );
   }
