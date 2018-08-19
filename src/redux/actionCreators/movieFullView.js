36bc05b76744068c800bfe5e530f3b06835e775f
Ian Izaguirre <ian@izaguir.re>
2018-08-19 00:02:48 -0400

    Add: not yet rated condition if certification returns empty string

--- src/redux/actionCreators/movieFullView.js
+++ src/redux/actionCreators/movieFullView.js
@@ -15,10 +15,13 @@ export function getMovieDetails(id) {
 function filterOutCertification(getReleaseDates) {
   // Retrieve array holding ALL "U.S." Objects which then gets Release Dates Array of Objects
   const isoUS = getReleaseDates.filter(item => item.iso_3166_1 === 'US')[0].release_dates;
-
   const typeTheatrical = isoUS.filter(item => item.type === 3)[0];
+  let movieCertification = typeTheatrical.certification;
 
-  const movieCertification = typeTheatrical.certification;
+  if (!movieCertification) {
+    // if Falsy
+    movieCertification = 'NR'; // No Rating Yet
+  }
 
   return movieCertification;
 }
